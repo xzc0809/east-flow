7f35b584e74aa46cc47d676189ca7cf86615244c
wdd15359180132 <1187818597@qq.com>
2020-08-13 17:41:59 +0800

    上传表单

--- src/components/template/uploadTemp.vue
+++ src/components/template/uploadTemp.vue
@@ -9,12 +9,60 @@
     <img v-if="imageUrl" :src="imageUrl" class="avatar">
     <i v-else class="el-icon-plus avatar-uploader-icon"></i>
   </el-upload>
+   <div>
+      <el-input placeholder="请输入内容" v-model="mediaUrl">
+        <template slot="prepend">图片地址</template>
+      </el-input>
+   </div>
   <div>
-    <el-input placeholder="请输入内容" v-model="input1">
+    <span style="float: left">标题及描述:</span>
+    <el-input placeholder="请输入内容" v-model="title">
       <template slot="prepend">标题</template>
     </el-input>
   </div>
-  </div>
+   <div>
+      <el-input placeholder="请输入内容" v-model="description">
+        <template slot="prepend">描述</template>
+      </el-input>
+    </div>
+    <span>链接：</span>
+    <el-input-number v-model="num" @change="handleChange()" :min="1" :max="3" label="描述文字"></el-input-number>
+    <div v-for="n in num">
+      <el-input placeholder="请输入内容" v-model="displayText">
+        <template slot="prepend">按钮名</template>
+      </el-input>
+      <el-input placeholder="请输入内容" v-model="url">
+        <template slot="prepend">链接</template>
+      </el-input>
+    </div>
+    <span>电话：</span>
+<!--    <el-input-number v-model="num2" @change="handleChange()" :min="1" :max="2" label="描述文字"></el-input-number>-->
+    <div v-for="n in num2">
+      <el-input placeholder="请输入内容" v-model="displayText2">
+        <template slot="prepend">按钮名</template>
+      </el-input>
+    <el-input placeholder="请输入内容" v-model="phoneNumber">
+      <template slot="prepend">电话</template>
+    </el-input>
+    </div>
+    <span>位置信息：</span>
+<!--    <el-input-number v-model="num3" @change="handleChange()" :min="1" :max="2" label="描述文字"></el-input-number>-->
+    <div v-for="n in num3">
+      <el-input placeholder="请输入内容" v-model="displayText3">
+        <template slot="prepend">按钮名</template>
+      </el-input>
+      <el-input placeholder="请输入内容" v-model="label">
+        <template slot="prepend">标题</template>
+      </el-input>
+    <el-input placeholder="请输入内容" v-model="longitude">
+      <template slot="prepend">经度</template>
+    </el-input>
+      <el-input placeholder="请输入内容" v-model="latitude">
+        <template slot="prepend">纬度</template>
+      </el-input>
+    </div>
+
+    </div>
 </template>
 <style>
   .avatar-uploader .el-upload {
@@ -51,7 +99,13 @@
     data () {
       return {
         imageUrl: '',
-        input1:''
+        input1:'',
+        num:1,
+        num2:1,
+        num3:1,
+        content:[{
+
+        }]
       }
     },
     methods: {
@@ -69,6 +123,8 @@
           this.$message.error('上传头像图片大小不能超过 2MB!')
         }
         return isJPG && isLt2M
+      },
+      handleChange(num) {
       }
     }
   }
